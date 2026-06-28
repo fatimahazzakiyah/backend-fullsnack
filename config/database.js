9cc791a72e02c7be0e2071904e1beb754cc600d1
fatimahazzakiyah <fatimahazzakiyah6@gmail.com>
2026-06-29 02:11:40 +0700

    fix database

--- config/database.js
+++ config/database.js
@@ -1,12 +1,20 @@
 const mysql = require("mysql2");
 
-const db = mysql.createConnection({
-  host: process.env.DB_HOST || "localhost",
-  user: process.env.DB_USER || "root",
-  password: process.env.DB_PASSWORD || "",
-  database: process.env.DB_NAME || "db_fullsnack",
-  port: process.env.DB_PORT || 3306,
-});
+const connectionUrl = process.env.MYSQL_PUBLIC_URL;
+
+let db;
+
+if (connectionUrl) {
+  db = mysql.createConnection(connectionUrl);
+} else {
+  db = mysql.createConnection({
+    host: process.env.DB_HOST || "localhost",
+    user: process.env.DB_USER || "root",
+    password: process.env.DB_PASSWORD || "",
+    database: process.env.DB_NAME || "db_fullsnack",
+    port: process.env.DB_PORT || 3306,
+  });
+}
 
 db.connect((err) => {
   if (err) {
