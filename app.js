08905f1ae07a74c9c21b57615ca6d8440569dc28
fatimahazzakiyah <fatimahazzakiyah6@gmail.com>
2026-06-28 23:02:01 +0700

    fix entry

--- app.js
+++ app.js
@@ -9,6 +9,8 @@ const app = express();
 app.use(
   cors({
     origin: "*",
+    methods: ["GET", "POST", "PUT", "DELETE"],
+    allowedHeaders: ["Content-Type", "Authorization"],
   }),
 );
 app.use(express.json());
